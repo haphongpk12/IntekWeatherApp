cf2151e7dd4c6e7fe4e770428321f7776d8a0cdc
tkhang <haphongpk12@gmail.com>
2019-08-14 18:36:16 +0700

    Done Project

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
-# IntekWeatherApp
+# IntekWeatherApp 
+
+#### Core
+
+### Mission
+
+You have to develop a React Native app that will work in both iOS and Android platforms. For convenience we are only going to test the app in iOS iPhone simulator. Your app will query a local JSON file and call a third party API to fetch the data and display in the app.
+
+### Open Weather Map API
+
+We are going to be using a third party API to get the required data. It requires you to create an account in them and subscribe you to their API. Follow the link below to register an account:
+
+https://openweathermap.org
+
+Once you are registered you need to subscribe to Current Weather data API in the following link:
+
+https://openweathermap.org/api
+
+You would have to read the API docs for the specific API. The following link helps you to understand API more:
+
+https://openweathermap.org/current
+
+### Prerequisite
+
+We assume that you have React Native installed in your PC's. But for students whose PC doesn't have React Native, you need to follow the procedures step by step to get React Native installed:
+
+https://www.npmjs.com/package/react-native
+
+https://facebook.github.io/react-native/docs/getting-started
+
+### Practicing with React Native
+
+You can practice your React Native code using an online code editor. The following code editors are popular with developers and you may use them to practice your React Native code:
+
+https://codesandbox.io/s/q4qymyp2l6
+
+https://codepen.io/necolas/pen/PZzwBR
+
+### The App
+
+The following is the screenshot of the app:
+
+![](https://i.imgur.com/NI5ICv2.png)
+
+### Waypoint 1: Create a skeleton React Native app
+
+Create a basic React Native app that displays a text or an image.
+
+### Waypoint 2: Parse JSON file
+
+Fetch information from parsed data.
+
+### Waypoint 3: Create and Load Picker Component
+
+Create a Picker component in your app. More information on Picker can be found here:
+
+https://facebook.github.io/react-native/docs/picker
+
+Once you create a Picker component, load the parsed JSON data value city into pickers. Remember only load the JSON value of city into picker.
+
+### Waypoint 4: Create TextInput Component
+
+Create a TextInput component above the picker component. More information on TextInput component can be found here.
+
+https://facebook.github.io/react-native/docs/textinput
+
+When a text value is entered in TextInput it will show the corresponding value in Picker. In a way TextInput and Picker are synchronized.
+
+![](https://i.imgur.com/DoFSBtz.png)
+
+### Waypoint 5: Fetch API data
+
+When a valid TextInput is entered or when a Picker value is selected we fetch the API data of the selected city from Open Weather Map API.
+
+### Waypoint 6: Display the weather data
+
+Display the data of the fetched API from Open Weather and stylize a component with images for each weather conditions to display interactive weather data.
+
+![](https://i.imgur.com/pFgMBHX.png)
+
+### Local JSON File
+
+For this project you will be working with JSON file that will has all the cities in Vietnam.
+[JSON File](city.list.json)
+
+---
+
+#### Bonus 
+
+### Facebook Login
+
+We will be using `react-native-fbsdk` to integrate Facebook login to work with our app. The following is the documentation on how to install and use it:
+
+https://github.com/facebook/react-native-fbsdk
+
+Note that installing a wrapper doesn't always work directly. You would have to search for and find the best possible solutions to make it work.
+
+![](https://i.imgur.com/8QZo6m3.png)
---
+++ reactWeatherApp/App.js
@@ -0,0 +1,153 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Icon,
+  Picker,
+  Image,
+  TextInput,
+  ActivityIndicator,
+  FlatList,
+  ImageBackground,
+} from 'react-native';
+import Constants from 'expo-constants';
+
+const data_json = require('./city_list.json');
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      data: [],
+    };
+  }
+  fetchData = value => {
+    this.setState({ name: value });
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${
+      this.state.name
+      }&APPID=992619451504426b6920c762daa6ac96`
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          data: responseJson.main,
+        });
+      })
+      .catch(error => console.log(error));
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Weather App</Text>
+        <Text style={styles.statistics}>Weather Statistics</Text>
+        <TouchableOpacity
+          onPress={this._onPressButton}
+          style={styles.touchableopacity}>
+          <Image
+            source={require('./icon-facebook-square-png.png')}
+            style={styles.logo}
+          />
+          <Text style={{ color: 'white', fontSize: 17 }}>
+            Continue with Facebook
+          </Text>
+        </TouchableOpacity>
+        <TextInput
+          style={styles.textinput}
+          placeholder="Input your city"
+          placeholderTextColor="#2d5291"
+          onChangeText={name => this.setState({ name })}
+          value={`${this.state.name}`}
+        />
+        <Picker
+          selectedValue={this.state.name}
+          style={styles.pickers}
+          onValueChange={itemValue => {
+            this.fetchData(itemValue);
+          }}>
+          {data_json.map(item => {
+            return (
+              <Picker.Item label={item.name} value={item.name} key={item.id} />
+            );
+          })}
+        </Picker>
+        <ImageBackground
+          source={{ uri: 'http://openweathermap.org/img/w/' + this.state.data.weather[0].icon + '.png' }}
+          style={styles.imageWeather}
+        />
+        <View style={styles.info}>
+          <Text style={styles.infotext}>
+            City: {this.state.data.name}
+            {'\n\n'}
+          </Text>
+          <Text style={styles.infotext}>
+            Temperature: {this.state.data.main.temp}
+          </Text>
+          <Text style={styles.infotext}>
+            Pressure: {this.state.data.main.pressure}
+          </Text>
+          <Text style={styles.infotext}>
+            Humidity: {this.state.data.main.humidity}
+          </Text>
+        </View>
+        ;
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'white',
+  },
+  header: {
+    backgroundColor: '#3399FF',
+    width: '100%',
+    height: 40,
+    color: 'white',
+    fontSize: 25,
+    textAlign: 'center',
+    paddingTop: 10,
+  },
+  statistics: {
+    margin: 18,
+    fontSize: 20,
+  },
+  touchableopacity: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#2d5291',
+    padding: 10,
+    borderRadius: 3,
+  },
+  pickers: {
+    width: 200,
+    height: 100,
+    marginTop: 30,
+  },
+  textinput: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 50,
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  logo: {
+    width: 25,
+    height: 25,
+    borderRadius: 2,
+    marginRight: 10,
+  },
+  info: {
+    marginTop: 150,
+  },
+  infotext: {
+    fontSize: 18,
+  },
+});
